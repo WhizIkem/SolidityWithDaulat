4afde56e5ae164cc9376343f0a34949bdd4f443b
WhizIkem <wisdomikem@gmail.com>
2024-08-23 16:26:13 +0100

    Update data directly on the state variable

---
+++ Hello.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+contract MyFirstContract {
+    //update data directly on the state variable
+    string public hey = "I'm Whiz";
+    uint public no = 500;
+}
