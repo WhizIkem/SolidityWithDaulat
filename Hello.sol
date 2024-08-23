d04f0661b12ef0912ecdc0ed15fec0627d4ee5a2
WhizIkem <wisdomikem@gmail.com>
2024-08-23 16:46:02 +0100

    update data by using a constructor

--- Hello.sol
+++ Hello.sol
@@ -3,6 +3,16 @@ pragma solidity ^0.8.20;
 
 contract MyFirstContract {
     //update data directly on the state variable
-    string public hey = "I'm Whiz";
+    //string public hey = "I'm Whiz";
+    //uint public no = 500;
+
+    //update data by using a constructor
+    string public hey;
     uint public no = 500;
+
+    constructor (string memory _hey, uint _no) {
+        // assigning to a state variable by using a constructor
+        hey = _hey;
+        no = _no;
+        }
 }
