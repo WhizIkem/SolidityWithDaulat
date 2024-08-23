770832ccdcfd8e4d2f612b2d277a6d5f4479265a
WhizIkem <wisdomikem@gmail.com>
2024-08-23 16:54:26 +0100

    update state variable with the help of a function

--- Hello.sol
+++ Hello.sol
@@ -6,13 +6,19 @@ contract MyFirstContract {
     //string public hey = "I'm Whiz";
     //uint public no = 500;
 
-    //update data by using a constructor
+    //update data by using a constructor and function
     string public hey;
     uint public no = 500;
 
-    constructor (string memory _hey, uint _no) {
+    //constructor (string memory _hey, uint _no) {
         // assigning to a state variable by using a constructor
+        //hey = _hey;
+        //no = _no;
+        //}
+
+    //Update state variable with the help of a function
+    function addData (string memory _hey, uint _no) public {
         hey = _hey;
         no = _no;
-        }
+    }   
 }
